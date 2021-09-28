48526aa3a99e65b99211bfcfe2b98b8980127dae
samuel <samuel@DESKTOP-GNPCORI.localdomain>
2021-09-28 01:30:56 -0300

    adição do mappings.json e algumas outras mudanças

--- projeto-mips/mips.py
+++ projeto-mips/mips.py
@@ -1,9 +1,20 @@
 import funcAux as fAux
+import json
 
-print(fAux.getOpCode("00000010000100010100000000100000") + " " + fAux.getRs("00000010000100010100000000100000") 
-    + " " + fAux.getRt("00000010000100010100000000100000") + " " + fAux.getRd("00000010000100010100000000100000")
-    + " " + fAux.getSh("00000010000100010100000000100000") + " " + fAux.getFn("00000010000100010100000000100000"))
+with open("mappings.json") as jsonFile:
+    # A variavel mappings tem os valores de um dictionary salvo como um json, 
+    # onde as keys mais externas sao o valor do opcode e dentro da key "000000",
+    # que seria opcode para tipo R, sao guardadas as keys com o valor do fn.
+    mappings = json.load(jsonFile)
+    jsonFile.close()
 
-print(fAux.getOffset("00000010000100010100000000100000") + " " + fAux.getJTA("00000010000100010100000000100000"))
+binary = fAux.hex_to_binary("0x02114020")
 
-print(fAux.hex_to_binary("0x02114020"))
+print(binary)
+
+def identificadorInst(str):
+    if fAux.getOpCode(str) == "000000":
+        opCode = fAux.getOpCode(str)
+        print(mappings[opCode][fAux.getFn(str)])
+
+identificadorInst(binary)
