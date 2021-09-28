54dc33decab838dac40006a0189293f0bb9a6861
samuel <samuel@DESKTOP-GNPCORI.localdomain>
2021-09-28 00:00:56 -0300

    funções uteis

--- projeto-mips/mips.py
+++ projeto-mips/mips.py
@@ -1,7 +1,5 @@
-
-#00000010000100010100000000100000
-
-# funcao que converte uma string de hexadecimal para binario
+# funcao que converte uma string de hexadecimal para binario.
+# exemplo: 0x02114020 -> 00000010000100010100000000100000
 def hex_to_binary(str):
     sliced = str[2:]
     end_length = len(sliced) * 4
@@ -10,5 +8,38 @@ def hex_to_binary(str):
     padded_binary = hex_as_binary[2:].zfill(end_length)
     return padded_binary
 
+# funcao que retorna o opcode de uma string binaria.
+# exemplo: 00000010000100010100000000100000 -> 000000
+def getOpCode(str):
+    return str[:6]
+
+# funcao que retorna o Rs de uma string binaria.
+# exemplo: 00000010000100010100000000100000 -> 10000
+def getRs(str):
+    return str[6:11]
+
+# funcao que retorna o Rt de uma string binaria.
+# exemplo: 00000010000100010100000000100000 -> 10001
+def getRt(str):
+    return str[11:16]
+
+# funcao que retorna o Rd de uma string binaria.
+# exemplo: 00000010000100010100000000100000 -> 01000
+def getRd(str):
+    return str[16:21]
+
+# funcao que retorna o Sh de uma string binaria.
+# exemplo: 00000010000100010100000000100000 -> 00000
+def getSh(str):
+    return str[21:26]
+
+# funcao que retorna o Fn de uma string binaria.
+# exemplo: 00000010000100010100000000100000 -> 100000
+def getFn(str):
+    return str[26:32]
+
+print(getOpCode("00000010000100010100000000100000") + " " + getRs("00000010000100010100000000100000") 
+    + " " + getRt("00000010000100010100000000100000") + " " + getRd("00000010000100010100000000100000")
+    + " " + getSh("00000010000100010100000000100000") + " " + getFn("00000010000100010100000000100000"))
 
 print(hex_to_binary("0x02114020"))
